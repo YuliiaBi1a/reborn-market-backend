8ab569e8c79783060375c599379a18c5d1b8a899
YuliiaBi1a <jasmin.ic.km.ua13@gmail.com>
2025-03-18 12:39:39 +0100

    Initial commit

---
+++ src/main/java/com/hacksisters/reborn_market_backend/RebornMarketBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hacksisters.reborn_market_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RebornMarketBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RebornMarketBackendApplication.class, args);
+	}
+
+}
